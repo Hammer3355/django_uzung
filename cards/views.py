f4b261b20dc76d75b3459fdd9c954ef5133ed524
Professional <ttkagentusolie@gmail.com>
2024-02-12 06:44:54 +0800

    Собственный urls.py в приложении cards и маршруты cards/catalog

--- cards/views.py
+++ cards/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("<h1>Привет, мир!</h1>")
 
+
+def catalog(request):
+    return HttpResponse("<h1>Каталог карточек</h1>")
