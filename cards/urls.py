4bbcb8ee4ea79f637a50637e99dea6950f3c0ea6
Professional <ttkagentusolie@gmail.com>
2024-02-12 12:36:12 +0800

    Создание маршрута slug

--- cards/urls.py
+++ cards/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('catalog/', views.catalog, name='catalog'),
     # Карточки по ID
     path('catalog/<int:card_id>/', views.get_card_by_id, name='card'),
+    # Карточки по slug
+    path('catalog/<slug:slug>', views.get_category_by_name, name='category'),
 ]
--- cards/views.py
+++ cards/views.py
@@ -4,9 +4,12 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("<h1>Привет, мир!</h1>")
 
-
 def catalog(request):
     return HttpResponse("<h1>Каталог карточек</h1>")
 
 def get_card_by_id(request, card_id):
     return HttpResponse(f"<h1>Карточка по ID: {card_id}</h1>")
+
+
+def get_category_by_name(request, slug):
+    return HttpResponse(f"<h1>Категория {slug}</h1>")
