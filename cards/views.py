5f27a85cba49c4f302020fc5576f92d5c2cc1932
Professional <ttkagentusolie@gmail.com>
2024-02-17 07:26:03 +0800

    Отладка теста. Тест пройден

--- cards/views.py
+++ cards/views.py
@@ -1,15 +1,15 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h1>Привет, мир!</h1>")
+    return HttpResponse("Привет, мир!")
 
 def catalog(request):
-    return HttpResponse("<h1>Каталог карточек</h1>")
+    return HttpResponse("Каталог карточек")
+
 
 def get_card_by_id(request, card_id):
-    return HttpResponse(f"<h1>Карточка по ID: {card_id}</h1>")
+    return HttpResponse(f"Карточка {card_id}")
 
 
 def get_category_by_name(request, slug):
-    return HttpResponse(f"<h1>Категория {slug}</h1>")
+    return HttpResponse(f"Категория {slug}")
